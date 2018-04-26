1a72edd9d773d52a33862ae7686389e6f9916537
Roey Darwish Dror <roeyd@infinidat.com>
2018-04-26 16:26:15 +0300

    Use more modern colors for the graph

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     data = pandas.read_csv(sys.argv[1])
-    red = plt.bar(data.index, data['red_player_cards'], 1, color='red')
-    blue = plt.bar(data.index, data['blue_player_cards'], 1, color='blue', bottom=data['red_player_cards'])
+    red = plt.bar(data.index, data['red_player_cards'], 1, color='#c0504e')
+    blue = plt.bar(data.index, data['blue_player_cards'], 1, color='#558ed3', bottom=data['red_player_cards'])
     plt.ylabel('Number of cards')
     plt.xlabel('Turns')
     plt.legend((red[0], blue[0]), ('First player cards', 'Second player cards'))
