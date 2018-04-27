b6099ca23b38975b5e0ee1c55482d035491f5f3b
Konrad Borowski <github@borowski.pw>
2018-04-27 10:10:37 +0200

    Reduce code duplication in game

--- src/game.rs
+++ src/game.rs
@@ -1,18 +1,19 @@
 use super::player::Player;
 use cards::{Card, Deck};
+use enum_map::EnumMap;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::vec::Vec;
 
-#[derive(Debug)]
+#[derive(Debug, EnumMap)]
 pub enum Players {
     Blue,
     Red,
 }
 
 pub struct Game {
-    blue_player: Player,
-    red_player: Player,
+    players: EnumMap<Players, Player>,
     wars: usize,
     turns: usize,
     output_file: Option<BufWriter<File>>,
@@ -45,24 +46,19 @@ impl Game {
         deck.shuffle();
 
         let mut game = Game {
-            blue_player: Player::new(),
-            red_player: Player::new(),
+            players: enum_map! { _ => Player::new() },
             turns: 0,
             wars: 0,
             output_file: output_file.map(|f| BufWriter::new(File::create(f).unwrap())),
         };
 
-        loop {
-            if let Some(card) = deck.draw() {
-                game.blue_player.claim(card)
-            } else {
-                break;
-            }
-
-            if let Some(card) = deck.draw() {
-                game.red_player.claim(card)
-            } else {
-                break;
+        'cards: loop {
+            for player in game.players.values_mut() {
+                if let Some(card) = deck.draw() {
+                    player.claim(card);
+                } else {
+                    break 'cards;
+                }
             }
         }
 
@@ -72,36 +68,33 @@ impl Game {
     }
 
     fn play_turn(&mut self) -> bool {
-        let mut stake: Vec<Card> = Vec::new();
+        let mut stake = Vec::new();
         let mut war = false;
 
         loop {
-            let blue_card = draw!(self.blue_player);
-            let red_card = draw!(self.red_player);
-
-            if blue_card.rank() > red_card.rank() {
-                self.blue_player.claim(blue_card);
-                self.blue_player.claim(red_card);
-                for card in stake.drain(..) {
-                    self.blue_player.claim(card);
+            assert_eq!(self.players.len(), 2);
+            let blue_card = draw!(self.players[Players::Blue]);
+            let red_card = draw!(self.players[Players::Red]);
+            match blue_card.rank().cmp(&red_card.rank()) {
+                Ordering::Greater => {
+                    self.win(Players::Blue, &mut stake, blue_card, red_card);
+                    break;
                 }
-                break;
-            } else if blue_card.rank() < red_card.rank() {
-                self.red_player.claim(blue_card);
-                self.red_player.claim(red_card);
-                for card in stake.drain(..) {
-                    self.red_player.claim(card);
+                Ordering::Less => {
+                    self.win(Players::Red, &mut stake, blue_card, red_card);
+                    break;
                 }
-                break;
-            } else {
-                war = true;
-                self.wars += 1;
+                Ordering::Equal => {
+                    war = true;
+                    self.wars += 1;
 
-                stake.push(blue_card);
-                stake.push(red_card);
-                for _ in 0..2 {
-                    stake.push(draw!(self.red_player));
-                    stake.push(draw!(self.blue_player));
+                    stake.push(blue_card);
+                    stake.push(red_card);
+                    for _ in 0..2 {
+                        for player in self.players.values_mut() {
+                            stake.push(draw!(player));
+                        }
+                    }
                 }
             }
         }
@@ -111,13 +104,22 @@ impl Game {
             f.write(
                 format!(
                     "{},{},{}\n",
-                    self.red_player.number_of_cards(),
-                    self.blue_player.number_of_cards(),
+                    self.players[Players::Red].number_of_cards(),
+                    self.players[Players::Blue].number_of_cards(),
                     war
                 ).as_bytes(),
             ).unwrap();
         }
         true
+    }
+
+    fn win(&mut self, player: Players, stake: &mut Vec<Card>, blue_card: Card, red_card: Card) {
+        let player = &mut self.players[player];
+        player.claim(blue_card);
+        player.claim(red_card);
+        for card in stake.drain(..) {
+            player.claim(card);
+        }
     }
 
     pub fn play(mut self) -> GameResults {
@@ -128,13 +130,11 @@ impl Game {
 
         while self.play_turn() {}
 
-        assert!(self.blue_player.has_lost() || self.red_player.has_lost());
-
-        let winner = if self.blue_player.has_lost() {
-            Players::Red
-        } else {
-            Players::Blue
-        };
+        let winner = self.players
+            .iter()
+            .find(|(_, player)| !player.has_lost())
+            .unwrap()
+            .0;
 
         GameResults {
             turns: self.turns,
